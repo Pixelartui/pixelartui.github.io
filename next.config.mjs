f604611c47e042d4963e11b74bfe421c12fd6b94
Yazed Jamal <yazedjamal@Yazeds-MacBook-Air.local>
2025-11-11 16:03:57 +0000

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+import nextra from "nextra";
+
+const withNextra = nextra({});
+
+export default withNextra({
+    reactStrictMode: true,
+    turbopack: {
+        resolveAlias: {
+            // Path to your `mdx-components` file with extension
+            "next-mdx-import-source-file": "/mdx-components.jsx",
+        },
+    },
+});
